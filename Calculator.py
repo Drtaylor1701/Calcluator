0f855a0843b920846cfd60e6e84e17c3dc2c9675
drtaylor1701 <trekker35@gmail.com>
2018-06-28 08:29:08 -0700

    Arithmetic

--- Calculator.py
+++ Calculator.py
@@ -1,12 +1,6 @@
 print("What type of math are you doing?")
 print("Press 1 for arithmetic. Examples include addition, subtraction, multiplication, and division problems.")
-
 mathType = input()
-
-def selectMath(mathType):
-    if mathType == 1:
-        print("You have chosen to do arithmetic.")
-        print("Press 1 for addition")
-        arithmeticType = input()
-        
-selectMath(mathType)
+if mathType == 1:
+    print("You are doing arithmetic.")
+    
