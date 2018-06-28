4a2d80c5ad8a018baa7b8fb0194b5933116a3569
drtaylor1701 <trekker35@gmail.com>
2018-06-28 08:29:53 -0700

    changed mathtype to string

--- Calculator.py
+++ Calculator.py
@@ -1,6 +1,7 @@
 print("What type of math are you doing?")
 print("Press 1 for arithmetic. Examples include addition, subtraction, multiplication, and division problems.")
 mathType = input()
-if mathType == 1:
+if mathType == "1":
     print("You are doing arithmetic.")
-    
+else:
+    print("I'm not yet programmed for this type of math.")
