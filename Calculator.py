4ff15609fd78709dd2c587c8fbf1b761106f018d
drtaylor1701 <trekker35@gmail.com>
2018-08-04 09:18:31 -0700

    added main module

--- Calculator.py
+++ Calculator.py
@@ -1,4 +1,9 @@
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
+
+def main():
+    problemType()
+
 def problemType():
     input("Are we working on story problems?")
-
-problemType()
