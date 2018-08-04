9784b445c264ed6cf9719917d1809d216b505ae2
drtaylor1701 <trekker35@gmail.com>
2018-08-04 13:07:14 -0700

    Okay, so it has output but it doesn't have the right answer, but at least there's output.

--- Calculator.py
+++ Calculator.py
@@ -1,18 +1,39 @@
-MathType = ""
+MathType = 0
+equation = 0
 def main():
   print("Welcome to Calculator!")
   selectMathType()
+  basicArithmetic()
 
 def selectMathType():
-    type = input(For arithmetic press 1. For geometry press 2. For measurement press 3.)
+    type = input("For arithmetic press 1. For geometry press 2. For measurement press 3. ")
     if type == 1:
         MathType = Arithmetic
     elif type == 2:
         MathType = Geometry
     elif type == 3:
         MathType = Measurement
-    else:
-        print("That is not a recognized math type at this time.")
+
+def basicArithmetic():
+    print("Please enter the first number in the equation. ")
+    firstNumber = input()
+    print("Please enter the second number in the equation. ")
+    secondNumber = input()
+    print("What operation are we doing? ")
+    print("Input + for addition, - for subtraction, * for multiplication, and / for division. ")
+    operation = input()
+    if operation == "+":
+        equation = firstNumber + secondNumber
+    elif operation == "-":
+        equation = firstNumber - secondNumber
+    elif operation == "*":
+        equation = firstNumber * secondNumber
+    elif operation == "/":
+        equation = firstNumber * secondNumber
+
+    print(equation)
+    return equation
+
 
 if __name__== "__main__":
   main()
