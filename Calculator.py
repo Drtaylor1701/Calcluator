f385fd31e268145a59f6fd653c0e5fb70caf01a2
drtaylor1701 <trekker35@gmail.com>
2018-08-04 09:27:00 -0700

    Selects math type

--- Calculator.py
+++ Calculator.py
@@ -1,5 +1,18 @@
+MathType = ""
 def main():
   print("Welcome to Calculator!")
+  selectMathType()
+
+def selectMathType():
+    type = input(For arithmetic press 1. For geometry press 2. For measurement press 3.)
+    if type == 1:
+        MathType = Arithmetic
+    elif type == 2:
+        MathType = Geometry
+    elif type == 3:
+        MathType = Measurement
+    else:
+        print("That is not a recognized math type at this time.")
 
 if __name__== "__main__":
   main()
