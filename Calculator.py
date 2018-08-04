c9b2c48f3c5a8bdfccb74d0ab3e9eaf9e91ef5c0
drtaylor1701 <trekker35@gmail.com>
2018-08-04 09:15:48 -0700

    Attempting to start the caclulator

--- Calculator.py
+++ Calculator.py
@@ -1,21 +1,4 @@
+def problemType():
+    input("Are we working on story problems?")
 
-def arithmetic(first, second, operation):
-    first = float(first)
-    second = float(second)
-    if operation == 1:
-        total = first + second
-        print(total)
-        return total
-
-print("What type of math are you doing?")
-print("Press 1 for arithmetic. Examples include addition, subtraction, multiplication, and division problems.")
-mathType = input()
-if mathType == "1":
-    print("You are doing arithmetic.")
-    first = input("What is the first number in your problem?" )
-    second = input("What is the second number in your problem?" )
-    operation = input("What are we doing with this? 1 for Addition" )
-
-    arithmetic(first, second, operation)
-else:
-    print("I'm not yet programmed for this type of math.")
+problemType()
